fa16cc50cac0afed3acfafe17bb73115f753479e
hsain9357 <hsain9357@gmail.com>
2022-09-06 13:27:24 +0300

    delete task fixed and modified server

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,10 +1,28 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
-);
+const STATIC_CATCHED = [
+  "android-chrome-192x192.png",
+  "android-chrome-512x512.png",
+  "apple-touch-icon.png",
+  "favicon-16x16.png",
+  "favicon-32x32.png",
+  "favicon.ico",
+  "maskable_icon_x192.png",
+  "index.html",
+  "https://fonts.googleapis.com/css2?family=Livvic:wght@400;500;600;700;900&display=swap",
+];
 
-workbox.precaching.precacheAndRoute([]);
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("todo-list").then((cache) => cache.addAll(STATIC_CATCHED))
+  );
+});
 
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|svg|json|js|jsx|html|css|woff|mp3)$/,
-  new workbox.strategies.NetworkFirst()
-);
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
